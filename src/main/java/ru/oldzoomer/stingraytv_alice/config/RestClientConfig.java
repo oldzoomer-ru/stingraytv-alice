c82eabc5417a9307cba453790254bd1642d5678c
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-12-27 20:21:39 +0300

    Change timeout limits to 3 seconds (required for Alice)

--- src/main/java/ru/oldzoomer/stingraytv_alice/config/RestClientConfig.java
+++ src/main/java/ru/oldzoomer/stingraytv_alice/config/RestClientConfig.java
@@ -1,20 +1,21 @@
 package ru.oldzoomer.stingraytv_alice.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Configuration
 public class RestClientConfig {
 
-    @Value("${app.rest-client.connect-timeout:5000}")
+    @Value("${app.rest-client.connect-timeout:3000}")
     private int connectTimeout;
 
-    @Value("${app.rest-client.read-timeout:10000}")
+    @Value("${app.rest-client.read-timeout:3000}")
     private int readTimeout;
 
     @Bean
